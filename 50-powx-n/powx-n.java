59f02e6df87c9ed00db7ddc468cc2495cf4c6492
whomHarsh1 <harshbinit12345@gmail.com>
2026-09-14 12:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (46.04%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long power = n;
+        if(power<0){
+            x=1/x;
+            power=-power;
+        }
+        double result =1;
+        while(power>0){
+            if(power%2==1){
+                result = result*x;
+            }
+            x=x*x;
+            power=power/2;
+        }
+        return result;
+    }
+}
